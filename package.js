1c26a45eb56f658f0eefab2f2d9180b267e2e7fd
Phuc Nguyen <contact@phucnguyen.info>
2016-03-03 12:42:57 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,17 +1,21 @@
 Package.describe({
   name: 'verso:archive-collection',
-  summary: 'Collection with all archived documents filtered out by default',
+  summary: 'Collection with all archived documents automatically filtered out from query result',
   version: '0.0.1',
   git: 'https://github.com/versolearning/meteor-archive-collection'
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.0');
+
   api.use([
-    'underscore@1.0.0',
-    'mongo@1.0.6',
+    'underscore',
+    'mongo',
     'minimongo',
-    'ecmascript'
+    'ecmascript@0.1.6'
   ]);
+
   api.add_files('archive-collection.js', ['client', 'server']);
+
   api.export('ArchiveCollection');
 });
